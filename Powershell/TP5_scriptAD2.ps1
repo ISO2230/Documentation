e3696a6663633e8cd278da04aaacc05edb76b188
ISO2230 <150447240+ISO2230@users.noreply.github.com>
2025-10-06 15:01:14 +0200

    Update TP5_scriptAD2.ps1

--- Powershell/TP5_scriptAD2.ps1
+++ Powershell/TP5_scriptAD2.ps1
@@ -5,7 +5,7 @@ Auteur CLB – 06/10/2025
 --------------------------------------------------------------------#>
 
 Import-Module ActiveDirectory
-$ADUsers=Import-csv "c:\users\Administrateur\Desktop\script\utilisateurs sodecaf.csv" -Delimiter ";"
+$ADUsers=Import-csv "c:\users\Administrateur\Documents\utilisateurs sodecaf.csv" -Delimiter ";"
 
 foreach ($user in $ADUsers) {
     $prenom=$user.firstname
@@ -15,26 +15,38 @@ foreach ($user in $ADUsers) {
     $login=$login.tolower()
     $password="Btssio2017"
 
-echo ($prenom+" "+$nom+" "+$fonction+" "+$login)
+    echo ($prenom+" "+$nom+" "+$fonction+" "+$login)
 
-#-------- Création des UO --------------
-if ((Get-ADOrganizationalUnit -filter "DistinguishedName -like 'ou=Employés,dc=sodecaf,dc=local'") -eq $null) {
-        New-ADOrganizationalUnit -Name "Employés" -Path "dc=sodecaf,dc=local" -ProtectedFromAccidentalDeletion $false
-}
+    #-------- Création des UO --------------
+    if ((Get-ADOrganizationalUnit -filter "DistinguishedName -like 'ou=Employés,dc=sodecaf,dc=local'") -eq $null) {
+            New-ADOrganizationalUnit -Name "Employés" -Path "dc=sodecaf,dc=local" -ProtectedFromAccidentalDeletion $false
+    }
  
-if ((Get-ADOrganizationalUnit -filter "DistinguishedName -like 'ou=$fonction,ou=Employés,dc=sodecaf,dc=local'") -eq $null) {
-        New-ADOrganizationalUnit -Name $fonction -Path "ou=Employés,dc=sodecaf,dc=local" -ProtectedFromAccidentalDeletion $false
-}
+    if ((Get-ADOrganizationalUnit -filter "DistinguishedName -like 'ou=$fonction,ou=Employés,dc=sodecaf,dc=local'") -eq $null) {
+            New-ADOrganizationalUnit -Name $fonction -Path "ou=Employés,dc=sodecaf,dc=local" -ProtectedFromAccidentalDeletion $false
+    }
 
-#-------- Création des groupes --------------
+    #-------- Création des groupes --------------
+    if (!(Get-ADGroup -Filter "DistinguishedName -like 'CN=$fonction,ou=Employés,dc=sodecaf,dc=local'")) {
+        New-ADGroup -Name $fonction -GroupScope Global -GroupCategory Security -Path "ou=$fonction,ou=Employés,dc=sodecaf,dc=local"
+    }
 
+    #-------- Création des comptes --------------
+    try {
+        New-ADUser -Name $nom_complet -GivenName $prenom -Surname $nom `
+        -SamAccountName $login `
+        -AccountPassword (ConvertTo-SecureString $password -AsPlainText -Force) `
+        -Path "ou=$fonction,ou=Employés,dc=sodecaf,dc=local" `
+        -Enabled $true
 
+        Add-ADGroupMember -Identity $fonction -Members $login
+    }
+    catch {
+        echo "L'utilisateur $login existe déjà"
+    }
 
-#-------- Création des comptes --------------
+    #-------- Intégration des comptes dans les groupes --------------
 
-
-
-#-------- Intégration des comptes dans les groupes --------------
 
 
 }
